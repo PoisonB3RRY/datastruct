b4a34a2b08815d94ef9654c1a5f15d4b84dfbe06
nickj <nickjianjian@outlook.com>
2021-07-22 23:42:17 +0800

    update node

--- linear_list/Node.c
+++ linear_list/Node.c
@@ -83,3 +83,29 @@ void CreateList_L(LinkList &L, int n)
         L->next = p;
     }
 }//CreateList_L
+
+void MergeList_L(LinkList &La, LinkList &Lb, LinkList &Lc)
+{
+    //已知单链线性表La和Lb的元素按值非递减排列
+    //归并La和Lb得到新的单链线性表Lc, Lc的元素也按值非递减排列
+    pa = La->next;
+    pb = Lb->next;
+    Lc = pc = La; //用La的头结点作为Lc的头结点
+    while (pa && pb)
+    {
+        if (pa->data <= pb->data)
+        {
+            pc->next = pa;
+            pc = pa;
+            pa = pa->next;
+        }
+        else
+        {
+            pc->next = pb;
+            pc = pb;
+            pb = pb->next;
+        }
+        pc->next = pa ? pa : pb; //插入剩余段
+        free(Lb); //释放Lb的头结点
+    }
+} //MergeList_L
