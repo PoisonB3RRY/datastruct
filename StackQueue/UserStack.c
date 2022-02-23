40177c31bcc3b9190eaa28bb04c0a18f0d0b4731
nickj <nickjianjian@outlook.com>
2022-02-24 00:25:34 +0800

    add some method of Stack

--- StackQueue/UserStack.c
+++ StackQueue/UserStack.c
@@ -6,17 +6,47 @@ struct SqStack S;
 
 void conversion()
 {
-  //对于输入的任意非负十进制整数，打印输出与其相等值的八进制数
-  InitStack(S);
-  scanf("%d", N);
-  while(N)
-  {
-    Push(S,N%8);
-    N=N/8;
-  }
-  while(!StackEmpty(s))
-  {
-    Pop(S,e);
-    printf("%d",e);
-  })
+    //对于输入的任意非负十进制整数，打印输出与其相等值的八进制数
+    InitStack(S);
+    scanf("%d", N);
+    while (N)
+    {
+        Push(S, N % 8);
+        N = N / 8;
+    }
+    while (!StackEmpty(s))
+    {
+        Pop(S, e);
+        printf("%d", e);
+    }
 }//conversion
+//将余数由低位至高位推进栈里，再有高位到低位输出
+
+void LineEdit(SqStack &s)
+{
+    InitStack(s);
+    c = getchar();
+    char ch;
+    while (c != EOF)
+    {
+        while (c != EOF && c != '\n')
+        {
+            switch (c)
+            {
+            case '#': Pop(s, ch);
+                break;
+            case '@': ClearStack(s);
+                break;
+            default: Push(s, c);
+                break
+            }
+            c = getchar();
+        }
+        ClearStack(s);
+        if (c != EOF) //如果c为换行符，则继续输入，否则摧毁堆栈退出这个方法
+        {
+            c = getchar();
+        }
+    }
+    DestroyStack(s);
+}//LineEdit
