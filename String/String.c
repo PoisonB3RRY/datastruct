5dd785963dff4c3f51ef8af10d07d812e5cd457c
PoisonB3RRY <34827477+PoisonB3RRY@users.noreply.github.com>
2022-02-28 23:20:17 +0800

    String update

--- String/String.c
+++ String/String.c
@@ -1,7 +1,8 @@
-#include<stdio.h>
-#include<stdlib.h>
 #include "status.h"
+#include <stdio.h>
+#include <stdlib.h>
+
 
 #define MAXSTRLEN 255 //用户可以在255以内定义最大串长
 
-typedef unsigned char SString[MAXSTRLEN+1]; //0号单元存放串的长度
+typedef unsigned char SString[MAXSTRLEN + 1]; // 0号单元存放串的长度
