c3baff118eb8553019ef0d7ec45913aea7ef590b
PoisonB3RRY <34827477+PoisonB3RRY@users.noreply.github.com>
2022-03-13 20:50:31 +0800

    Array Update

--- ArrayAndTable/Array.c
+++ ArrayAndTable/Array.c
@@ -1,13 +1,64 @@
-#include <stdarg.h>	//标准头文件，提供宏va_start、va_arg和va_end，用于存取变长参数表
 #include "status.h"
+#include <stdarg.h> //标准头文件，提供宏va_start、va_arg和va_end，用于存取变长参数表
+#include <stdio.h>
+#include <stdlib.h>
+
 #define MAX_ARRAY_DIM 8 //数组的最大维度为8
 
 typedef struct {
-    ElemType *base;		//数组元素基址，由InitArray分配
-    int dim;	//数组维数
-    int *bound;		//数组维界基址，由InitArray分配
-    int *constants;		//数组映象函数常量基址，由InitArray分配
+  ElemType *base; //数组元素基址，由InitArray分配
+  int dim;        //数组维数
+  int *bound;     //数组维界基址，由InitArray分配
+  int *constants; //数组映象函数常量基址，由InitArray分配
 } Array;
 
 Status InitArray(Array &A, int dim, ...);
 //若维数dim和随后的各维度长度合法，则构造相应的数组A，并返回OK；
+
+Status DestroyArray(Array &A);
+//销毁数组A
+
+Status Value(Array A, ElemType &e, ...);
+// A是n维数组，e为元素变量，随后是n个下表值；
+//若各下标不超界，则将e的值赋为所指定的A的元素，并返回ok
+
+Status Assign(Array &A, int dim, ...);
+// A是n维数组，e为元素变量，随后是n个下标值；
+//若下标不超界，则将e的值赋给所指定的A的元素值，并返回OK
+
+Status InitArray(Array &A, int dim, ...) {
+  //若维数dim和各维长度合法，则构造相应的数组A，并返回OK
+  if (dim < 1 || dim > MAX_ARRAY_DIM) {
+    return ERROR;
+  }
+  A.dim = dim;
+  A.bound = (int *)malloc(dim * sizeof(int));
+  if (!A.bound) {
+    exit(OVERFLOW);
+  }
+  //若各维长度合法，则存储A.bounds，并求出A的元素总数elemtotal
+  int elemtotal = 1;
+  va_start(ap, dim); // ap为va_list类型，是存放变长参数表信息的数组
+  for (int i = 0; i < dim, ++i) {
+    A.bound[i] = va_arg(ap, int);
+    if (A.bounds[i] < 0) {
+      return UNDERFLOW;
+    }
+    elemtotal *= A.bound[i];
+  }
+  va_end(ap);
+  A.base(ElemType *) malloc(elemtotal * sizeof(ElemType));
+  if (!A.base) {
+    exit(OVERFLOW);
+  }
+  //求映像函数的常数Ci，并存入A.constants[i-1],i=1,...,dim
+  A.constants = (int *)malloc(dim * sizeof(int));
+  if (!A.constants) {
+    exit(OVERFLOW);
+  }
+  A.constants[dim - 1] = 1; // L=1，指针的增减以元素的大小为单位
+  for (int i = dim - 1; i >= 0; --i) {
+    A.constants[i] = A.bound[i + 1] * A.constants[i + 1];
+  }
+  return OK;
+}
