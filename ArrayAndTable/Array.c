a6d5571d9e9a1d169c3af685f316c798ce6fb9eb
PoisonB3RRY <34827477+PoisonB3RRY@users.noreply.github.com>
2022-03-13 23:51:38 +0800

    Array Update

--- ArrayAndTable/Array.c
+++ ArrayAndTable/Array.c
@@ -6,10 +6,10 @@
 #define MAX_ARRAY_DIM 8 //数组的最大维度为8
 
 typedef struct {
-  ElemType *base; //数组元素基址，由InitArray分配
-  int dim;        //数组维数
-  int *bound;     //数组维界基址，由InitArray分配
-  int *constants; //数组映象函数常量基址，由InitArray分配
+    ElemType *base; //数组元素基址，由InitArray分配
+    int dim;        //数组维数
+    int *bound;     //数组维界基址，由InitArray分配
+    int *constants; //数组映象函数常量基址，由InitArray分配
 } Array;
 
 Status InitArray(Array &A, int dim, ...);
@@ -27,38 +27,70 @@ Status Assign(Array &A, int dim, ...);
 //若下标不超界，则将e的值赋给所指定的A的元素值，并返回OK
 
 Status InitArray(Array &A, int dim, ...) {
-  //若维数dim和各维长度合法，则构造相应的数组A，并返回OK
-  if (dim < 1 || dim > MAX_ARRAY_DIM) {
-    return ERROR;
-  }
-  A.dim = dim;
-  A.bound = (int *)malloc(dim * sizeof(int));
-  if (!A.bound) {
-    exit(OVERFLOW);
-  }
-  //若各维长度合法，则存储A.bounds，并求出A的元素总数elemtotal
-  int elemtotal = 1;
-  va_start(ap, dim); // ap为va_list类型，是存放变长参数表信息的数组
-  for (int i = 0; i < dim, ++i) {
-    A.bound[i] = va_arg(ap, int);
-    if (A.bounds[i] < 0) {
-      return UNDERFLOW;
+    //若维数dim和各维长度合法，则构造相应的数组A，并返回OK
+    if (dim < 1 || dim > MAX_ARRAY_DIM) {
+        return ERROR;
     }
-    elemtotal *= A.bound[i];
-  }
-  va_end(ap);
-  A.base(ElemType *) malloc(elemtotal * sizeof(ElemType));
-  if (!A.base) {
-    exit(OVERFLOW);
-  }
-  //求映像函数的常数Ci，并存入A.constants[i-1],i=1,...,dim
-  A.constants = (int *)malloc(dim * sizeof(int));
-  if (!A.constants) {
-    exit(OVERFLOW);
-  }
-  A.constants[dim - 1] = 1; // L=1，指针的增减以元素的大小为单位
-  for (int i = dim - 1; i >= 0; --i) {
-    A.constants[i] = A.bound[i + 1] * A.constants[i + 1];
-  }
-  return OK;
+    A.dim = dim;
+    A.bound = (int *)malloc(dim * sizeof(int));
+    if (!A.bound) {
+        exit(OVERFLOW);
+    }
+    //若各维长度合法，则存储A.bounds，并求出A的元素总数elemtotal
+    int elemtotal = 1;
+    va_start(ap, dim); // ap为va_list类型，是存放变长参数表信息的数组
+    for (int i = 0; i < dim, ++i) {
+        A.bound[i] = va_arg(ap, int);
+        if (A.bounds[i] < 0) {
+            return UNDERFLOW;
+        }
+        elemtotal *= A.bound[i];
+    }
+    va_end(ap);
+    A.base(ElemType *) malloc(elemtotal * sizeof(ElemType));
+    if (!A.base) {
+        exit(OVERFLOW);
+    }
+    //求映像函数的常数Ci，并存入A.constants[i-1],i=1,...,dim
+    A.constants = (int *)malloc(dim * sizeof(int));
+    if (!A.constants) {
+        exit(OVERFLOW);
+    }
+    A.constants[dim - 1] = 1; // L=1，指针的增减以元素的大小为单位
+    for (int i = dim - 1; i >= 0; --i) {
+        A.constants[i] = A.bound[i + 1] * A.constants[i + 1];
+    }
+    return OK;
+}
+
+Status DestoryArray(Array &A){
+    //销毁数组A
+    if(!A.base){
+        return ERROR;
+    }
+    free(A.base);
+    A.base=NULL;
+    if!(A.bound){
+        retuen ERROR;
+    }
+    free(A.bound);
+    A.bound=NULL;
+    if!(A.constants){
+        return ERROR;
+    }
+    A.constants=NULL;
+    return OK;
+}
+
+Status Locate(Array A, va_list ap, int &off){
+    //若ap指标的格下标值合法，则求出该元素在A中的相对地址off
+    off=0;
+    for(int i=0; i<A.dim; i++){
+        ind=va_arg(ap,int);
+        if(ind<0 || ind>=A.bound[i]){
+            return OVERFLOW;
+        }
+        off+=A.constants[i]*ind;
+    }
+    return OK;
 }
