76b42652fc5c3dcd9e2c9a8bd6724e1c8752b9be
PoisonB3RRY <34827477+PoisonB3RRY@users.noreply.github.com>
2022-03-20 20:45:56 +0800

    General List

--- ArrayAndTable/Array.c
+++ ArrayAndTable/Array.c
@@ -96,5 +96,23 @@ Status Locate(Array A, va_list ap, int &off){
 }
 
 Status Value(Array A, ElemType &e, ...){
+    //A是n维数组，e是元素变量,随后是n个下标值。
+    //若各下标不超界，则e赋值为所指定的A的元素值，并返回OK。
+    va_start(ap,e);
+    if((result=Locate(A,ap,off))<=0){
+        return 0;
+    }
+    e=*(A.base+off);
+    return OK;
+}
 
+Status Assign(Array &A, ElemType e, ...){
+    //A是n维数组，e为元素变量,随后是n个下标值
+    //若下标不超界，则将e的值赋给所指定的A的元素，并返回OK
+    va_start(ap,e);
+    if((result=Locate(A,ap,off))<=0){
+        return result;
+    }
+    *(A.base+off)=e;
+    return OK;
 }
