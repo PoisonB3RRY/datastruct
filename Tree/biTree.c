b49b76fa929fa67263debe9f5b068d79526877ac
nickj <nickjianjian@outlook.com>
2022-06-05 17:25:03 +0800

    Binary Tree Updated

--- Tree/biTree.c
+++ Tree/biTree.c
@@ -19,3 +19,51 @@ Status InOrderTreverse(BiTree T, Status (*visit)(TElemType e));
 Status PostOrderTreverse(BiTree T, Status (*visit)(TElemType e));
 
 Status LevelOrderTreverse(BiTree T, Status (*visit)(TElemType e));
+
+Status CreateBiTree(BiTree &T)
+{
+    TElemType tempdata;
+    scanf("%d", &tempdata);
+
+    if (tempdata == -1)
+    {
+        T = NULL;
+    }
+    else
+    {
+        T = (BiTNode)malloc(sizeof(BiTNode));
+        T.data = tempdata;
+        CreateBiTree(&(T.lchild));
+        CreateBiTree(&(T.rchild));
+    }
+    return OK;
+}
+
+Status PreOrderTreverse(BiTree T, Status (*visit)(TElemType e))
+{
+    visit = &PrintElement;
+    if (T)
+    {
+        if (visit(T->data))
+        {
+            if (PreOrderTreverse(T->lchild, visit))
+            {
+                if (PreOrderTreverse(T->rchild, visit))
+                {
+                    return OK;
+                }
+            }
+        }
+        return ERROR;
+    }
+    else
+    {
+        return OK;
+    }
+}
+
+Status PrintElement(TElemType e)
+{
+    printf(e);
+    return OK;
+}
