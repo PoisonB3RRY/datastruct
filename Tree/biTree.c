333f2a8c66751653dd019d3ab024ae0f3927dded
nickj <nickjianjian@outlook.com>
2022-06-06 13:44:28 +0800

    InOrderTreverse BiTree

--- Tree/biTree.c
+++ Tree/biTree.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdio.h>
 #include "status.h"
+#include "../StackQueue/SqStack.c"
 
 typedef int TElemType;
 
@@ -39,8 +40,16 @@ Status CreateBiTree(BiTree &T)
     return OK;
 }
 
+Status PrintElement(TElemType e)
+{
+    printf(e);
+    return OK;
+}
+
 Status PreOrderTreverse(BiTree T, Status (*visit)(TElemType e))
 {
+    //采用二叉链表存储结构，visit是对数据元素操作的应用函数
+    //先序遍历二叉树T的递归算法，对每个数据元素调用visit;
     visit = &PrintElement;
     if (T)
     {
@@ -62,8 +71,30 @@ Status PreOrderTreverse(BiTree T, Status (*visit)(TElemType e))
     }
 }
 
-Status PrintElement(TElemType e)
+Status InOrderTreverse(BiTree T, Status (*visit)(TElemType e))
 {
-    printf(e);
+    //中序遍历二叉树的非递归算法，利用栈
+    visit = &PrintElement;
+    SqStack S;
+    InitStack(S);
+    BiTNode *p = &T;
+    Push(S, p); //根指针进栈
+    while (!StackEmpty(S))
+    {
+        while (GetTop(S, p) && p)
+        {
+            Push(S, p->lchild); //向左走到尽头
+        }
+        Pop(S, p); //最后一个空指针退栈
+        if (!StackEmpty(S))
+        {
+            Pop(S, p);
+            if (!visit(p->data)) //访问节点，向右一步
+            {
+                return ERROR;
+            }
+            Push(S, p->rchild);
+        }
+    }
     return OK;
 }
